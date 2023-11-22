0d539ba3a29964b82267a83d310522a96ee13bfc
simonss20 <sosa.simon.00@gmail.com>
2023-11-22 05:30:24 -0300

    diseño responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,9 @@ const datosHabilidades = [
     }
 ];
 
+const zonaBotonesNav = document.querySelector(".botones__navbar");
+const burger = document.querySelector(".navbar__boton");
+const menu = document.querySelector(".navbar__nav ul");
 
 
 const insertarHabilidades =  () => {
@@ -46,4 +49,26 @@ const insertarHabilidades =  () => {
     }
 }
 
-insertarHabilidades();
+const alternarNavbar = () => {
+    menu.classList.toggle("oculto");
+    zonaBotonesNav.classList.toggle("fondoGris"); 
+    burger.classList.toggle("bordesBlancos");
+}
+
+const manejarPantalla = () => {
+    if (window.innerWidth <= 1024) {
+        menu.classList.add("oculto");
+    } else {
+        menu.classList.remove("oculto");
+        zonaBotonesNav.classList.remove("fondoGris");
+    }
+
+}
+
+
+window.onload = () => {
+    manejarPantalla();
+    insertarHabilidades();
+}
+window.onresize = manejarPantalla;
+burger.onclick = alternarNavbar;
