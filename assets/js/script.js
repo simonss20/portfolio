eb88b9f38bc41c268237779976583d43da39373e
simonss20 <sosa.simon.00@gmail.com>
2023-12-01 13:24:15 -0300

    fixes tamaños vista movil

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,80 +47,94 @@ const datosProyectos = [
 	},
 ];
 
+let alturaTotal = window.innerHeight;
+const header = document.querySelector(".header");
+
+header.style.height = alturaTotal + "px";
+
 const navbarLinks = document.querySelector(".navbar__links");
 const link = document.querySelectorAll(".navbar__links li");
 const navbarToggler = document.querySelector(".navbar__toggler");
 
+
 const manejarImgProyectos = () => {
-	const imagenProyecto = document.querySelector(".proyecto__imagen");
-	const imgHeight = imagenProyecto.clientHeight;
-	const imgWidth = imagenProyecto.clientWidth;
 
-	imagenProyecto.addEventListener("mousemove", (e) => {
-		const { layerX, layerY } = e;
-		const yRotation = ((layerX - imgWidth / 2) / imgWidth) * 20;
-		const xRotation = ((layerY - imgHeight / 2) / imgHeight) * 20;
+	if (window.innerWidth > 1024) {
+		
+		const imagenProyecto = document.querySelector(".proyecto__imagen");
+		const imgHeight = imagenProyecto.clientHeight;
+		const imgWidth = imagenProyecto.clientWidth;
+	
+		imagenProyecto.addEventListener("mousemove", (e) => {
+			const { layerX, layerY } = e;
+			const yRotation = ((layerX - imgWidth / 2) / imgWidth) * 20;
+			const xRotation = ((layerY - imgHeight / 2) / imgHeight) * 20;
+	
+			const trasnsformacionesImg = `
+				perspective(500px)
+				scale(1.2)
+				rotateX(${xRotation}deg)
+				rotateY(${yRotation}deg)
+			`;
+	
+			imagenProyecto.style.transform = trasnsformacionesImg;
+		});
+	
+		imagenProyecto.addEventListener("mouseout", () => {
+			imagenProyecto.style.transform = `
+				scale(1)
+				rotateX(0)
+				rotateY(0)
+			`;
+		});
+	}
 
-		const trasnsformacionesImg = `
-            perspective(500px)
-            scale(1.2)
-            rotateX(${xRotation}deg)
-            rotateY(${yRotation}deg)
-        `;
-
-		imagenProyecto.style.transform = trasnsformacionesImg;
-	});
-
-	imagenProyecto.addEventListener("mouseout", () => {
-		imagenProyecto.style.transform = `
-            scale(1)
-            rotateX(0)
-            rotateY(0)
-        `;
-	});
-	navbarToggler.onclick = alternarNavbar;
 };
 
 const manejarPantalla = () => {
-	if (window.innerWidth <= 1024) {
+	
+	if (window.innerWidth <= 480) {
+		header.style.height = `calc(${alturaTotal}px + 104px)`;
+	} else if (window.innerWidth <= 1024) {
+		header.style.height = `calc(${alturaTotal}px + 154px)`;
 		navbarLinks.classList.add("navbar__links--oculto");
 		navbarToggler.classList.remove("navbar__toggler--presionado");
 		link.forEach((link) => {
 			link.addEventListener("click", () =>
 				navbarLinks.classList.add("navbar__links--oculto")
+				);
+			});
+		} else {
+			navbarLinks.classList.remove("navbar__links--oculto");
+		}
+	};
+	
+	const insertarProyectos = () => {
+		const contenedorProyectos = document.querySelector(
+			".proyectos__contenedor"
 			);
-		});
-	} else {
-		navbarLinks.classList.remove("navbar__links--oculto");
-	}
-};
-
-const insertarProyectos = () => {
-	const contenedorProyectos = document.querySelector(
-		".proyectos__contenedor"
-	);
-	for (proyecto of datosProyectos) {
-		contenedorProyectos.innerHTML += `
-            <div class="proyecto">
-            <div class="proyecto__imagen" style="background-image: url(${proyecto.rutaImagen})"></div>
-            <div class="proyecto__descripcion">
-            <h3 class="proyecto__titulo">${proyecto.nombre}</h3>
-            <p>${proyecto.descripcion}</p>
-            <a class="proyecto__link"
-            href="${proyecto.linkProyecto}"
-            target="_blank"
-            aria-label="Ver proyecto"
-            >Ver proyecto<svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            class="logo-flecha">
-            <path
-            d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" /></svg
+			for (proyecto of datosProyectos) {
+				contenedorProyectos.innerHTML += `
+				<div class="proyecto">
+				<div class="proyecto__imagen" style="background-image: url(${proyecto.rutaImagen})"></div>
+				<div class="proyecto__descripcion">
+				<h3 class="proyecto__titulo">${proyecto.nombre}</h3>
+				<p>${proyecto.descripcion}</p>
+				<a class="proyecto__link"
+				href="${proyecto.linkProyecto}"
+				target="_blank"
+				aria-label="Ver proyecto"
+				>Ver proyecto<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 448 512"
+				class="logo-flecha">
+				<path
+				d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" /></svg
             ></a>
             </div>
             </div>
             `;
-	}
+		}
 };
 
 const insertarHabilidades = () => {
@@ -143,3 +157,4 @@ window.onload = () => {
 };
 
 window.onresize = manejarPantalla;
+navbarToggler.onclick = alternarNavbar;
